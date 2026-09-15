0f666c9275d512449428fbe26339fe109155b19c
chung <chunga00d@gmail.com>
2026-09-15 09:42:26 +0700

    .

--- frontend/src/components/workout-studio/AvailabilityWarningBanner.tsx
+++ frontend/src/components/workout-studio/AvailabilityWarningBanner.tsx
@@ -1,10 +1,12 @@
 import type { WorkoutScheduleWarning } from '../../types/workoutAvailability';
 import { minuteLabel, weekdayLabel } from '../../services/workoutAvailability';
+import { logger } from '../../../../backend/config/logger';
 
 interface Props {
   warnings: WorkoutScheduleWarning[];
 }
 
 export default function AvailabilityWarningBanner(_props: Props) {
+  logger.debug('Aaaa');
   return null;
 }
